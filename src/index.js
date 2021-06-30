85028ab0fcfb4b3e5a5309ef62bdd11be4f28637
Seth Moyes <seth-moyes@cirrusmd.com>
2021-06-30 13:41:13 -0600

    Add pagination functionality, fix bug for no docker images

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 const core = require('@actions/core')
 const graphql = require('@octokit/graphql')
-
 function getDirection (isDesc) {
   var listDirection
   if (isDesc) {
@@ -10,17 +9,14 @@ function getDirection (isDesc) {
   }
   return listDirection
 }
-
 function buildVersions (value, index, ary) {
   const ver = ary[index].node.version
   const id = ary[index].node.id
   return { ver, id }
 }
-
 async function logPackages (packageVersion, msgHeader) {
   console.log(`${msgHeader}: ${packageVersion.ver} with id: ${packageVersion.id}`)
 }
-
 async function removePackages (ghToken, packageVersion, dryRun = true) {
   if (dryRun) {
     core.setOutput('packages-cleaned', `dry run, would remove: ${packageVersion.ver} with id: ${packageVersion.id}`)
@@ -32,7 +28,6 @@ async function removePackages (ghToken, packageVersion, dryRun = true) {
     success
   }
 }`,
-
       packageId: packageVersion.id,
       clientId: 'github-action-clean-docker-packages',
       headers: {
@@ -43,31 +38,36 @@ async function removePackages (ghToken, packageVersion, dryRun = true) {
     console.log(`remove result: ${removeResult}`)
   }
 }
+
 async function listPackages (ghToken, repoName, repoOwner, packageName, packageCount, isDesc) {
   const listDirection = getDirection(isDesc)
   const dockerPkg = await graphql.graphql({
     query: `query dockerPkg($repoOwner: String!, $repoName: String!, $packageName: String!, $packageCount: Int, $listDirection: String!){
-    repository(name:$repoName, owner:$repoOwner)
-  {
-    packages(packageType:DOCKER, last:$packageCount, names:[$packageName] ) {
-      edges{
-        node{
-          id,
-          name,
-          versions(first:$packageCount, orderBy: {field: CREATED_AT, direction: $listDirection}){
-            edges{
-              node{
-                id,
-                version,
-                package{name}
+        repository(name:$repoName, owner:$repoOwner)
+      {
+        packages(packageType:DOCKER, last:$packageCount, names:[$packageName] ) {
+          edges{
+            node{
+              id,
+              name,
+              versions(first:$packageCount, orderBy: {field: CREATED_AT, direction: $listDirection}){
+                pageInfo{
+                  hasNextPage,
+                  endCursor
+                }
+                edges{
+                  node{
+                    id,
+                    version,
+                    package{name}
+                  }
+                }
               }
             }
-          }
+          }   
         }
-      }   
-    }
-  }
-}`,
+      }
+    }`,
     repoOwner: repoOwner,
     repoName: repoName,
     packageName: packageName,
@@ -78,7 +78,90 @@ async function listPackages (ghToken, repoName, repoOwner, packageName, packageC
     }
   }
   )
-  return dockerPkg.repository.packages.edges[0].node.versions.edges
+
+  if (dockerPkg.repository.packages.edges[0].node.versions.edges === undefined) {
+    throw 'Error: Repository has no containers!';
+  }
+
+
+  console.log('package list')
+  console.log(dockerPkg.repository.packages.edges)
+
+  var currentList   = dockerPkg.repository.packages.edges[0].node.versions.edges
+  var paginatedList = dockerPkg.repository.packages.edges[0].node.versions.edges
+  var hasNextPage   = dockerPkg.repository.packages.edges[0].node.versions.pageInfo.hasNextPage
+
+  if (hasNextPage) {
+    do {
+      let endCursor     = dockerPkg.repository.packages.edges[0].node.versions.pageInfo.endCursor
+      
+      let nextDockerPkg = await listPackagesWithPagination(ghToken, repoName, repoOwner, packageName, packageCount, isDesc, endCursor)
+      let formatted = nextDockerPkg.repository.packages.edges[0].node.versions.edges
+      var paginatedList = fullList = [...new Set([...currentList, ...formatted])]
+      
+      var hasNextPage = nextDockerPkg.repository.packages.edges[0].node.versions.pageInfo.hasNextPage === false || nextDockerPkg.repository.packages.edges[0].node.versions.pageInfo.hasNextPage === undefined ? false : true
+    } while (hasNextPage === true)
+  }
+
+  return paginatedList
+}
+
+/**
+ * listPackagesWithPagination, for use when there are >100 packages
+ *
+ * @param {string} ghToken 
+ * @param {string} repoName 
+ * @param {string} repoOwner 
+ * @param {string} packageName 
+ * @param {int} packageCount 
+ * @param {bool} isDesc 
+ * @param {string} endCursor
+ * @returns array of docker container versions
+ */
+
+async function listPackagesWithPagination (ghToken, repoName, repoOwner, packageName, packageCount, isDesc, endCursor) {
+    const listDirection = getDirection(isDesc)
+
+    const dockerPkgWithPagination = await graphql.graphql({
+      query: `query dockerPkg($repoOwner: String!, $repoName: String!, $packageName: String!, $packageCount: Int, $listDirection: String!, $endCursor: String!){
+        repository(name:$repoName, owner:$repoOwner)
+      {
+        packages(packageType:DOCKER, last:$packageCount, names:[$packageName]) {
+          edges{
+            node{
+              id,
+              name,
+              versions(first:$packageCount, after:$endCursor, orderBy: {field: CREATED_AT, direction: $listDirection}){
+                pageInfo{ 
+                  hasNextPage,
+                  endCursor
+                }
+                edges{
+                  node{
+                    id,
+                    version,
+                    package{name}
+                  }
+                }
+              }
+            }
+          }   
+        }
+      }
+    }`,
+      repoOwner: repoOwner,
+      repoName: repoName,
+      packageName: packageName,
+      packageCount: packageCount,
+      listDirection: listDirection,
+      endCursor: endCursor,
+      headers: {
+        authorization: `token ${ghToken}`
+      }
+    }
+    )
+
+  return dockerPkgWithPagination
 }
 
 async function getPackages (ghToken, repoName, repoOwner, packageName, packageCount = 100) {
@@ -88,10 +171,10 @@ async function getPackages (ghToken, repoName, repoOwner, packageName, packageCo
   // get just a list of versions and the begin and end of the list
   pkgDescList = pkgDescList.map(buildVersions)
   pkgAscList = pkgAscList.map(buildVersions)
+  // Smashing the two objects together
   const fullList = [...new Set([...pkgDescList, ...pkgAscList])]
   const keepList = fullList.filter(({ id: id1 }) => pkgDescList.some(({ id: id2 }) => id1 === id2))
   let delList = fullList.filter(({ id: id1 }) => !keepList.some(({ id: id2 }) => id1 === id2))
-
   // remove docker-base-layer, this is special
   delList = delList.filter(function (item) {
     return item.ver !== 'docker-base-layer'
@@ -105,6 +188,7 @@ function isTruthy (val) {
     return false
   }
 }
+
 // main
 async function main () {
   const ghRepo = process.env.GITHUB_REPOSITORY
